7516ad69d27d2754af35ac0cf6aaa7825fe86e47
Dhanesh <115584498+dhaneshkoti@users.noreply.github.com>
2024-11-10 22:56:52 +0530

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,10 @@
+a = float(input("number1: "))
+b = float(input("number2: "))
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+if(b == 0):
+	print("Division: Cannot divide by zero")
+
+else:
+	print("Division:",a/b)
